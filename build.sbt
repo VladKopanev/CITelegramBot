5d8d668d7e8784608716681b1a272b8c0860ce16
VladKopanev <ivengo53@gmail.com>
2022-01-29 19:56:50 +0200

    Update depndencies
    Signed-off-by: VladKopanev <ivengo53@gmail.com>

--- build.sbt
+++ build.sbt
@@ -1,14 +1,14 @@
 name := "CITelegramBot"
 
-scalaVersion := "3.1.0"
+scalaVersion := "3.1.1"
 
-val CatsVersion = "3.1.1"
-val Http4sVersion = "0.23.3"
+val CatsVersion = "3.3.4"
+val Http4sVersion = "0.23.7"
 val CirceVersion = "0.14.1"
-val SttpVersion = "3.3.17"
-val Log4CatsVersion = "2.1.1"
+val SttpVersion = "3.4.0"
+val Log4CatsVersion = "2.2.0"
 val DoobieVersion = "1.0.0-RC1"
-val CirisVersion = "2.2.0"
+val CirisVersion = "2.3.1"
 
 maintainer := "VladKopanev"
 
@@ -23,13 +23,12 @@ libraryDependencies ++= Seq(
   "org.typelevel" %% "cats-effect"         % CatsVersion,
   "org.typelevel" %% "log4cats-slf4j"      % Log4CatsVersion,
   "is.cir"        %% "ciris"               % CirisVersion,
-  "is.cir"        %% "ciris-refined"       % CirisVersion,
   "com.softwaremill.sttp.client3" %% "async-http-client-backend-cats" % SttpVersion
 ).map(_.cross(CrossVersion.for3Use2_13))
 
 libraryDependencies ++= Seq(
   "com.davegurnell" %% "unindent"       % "1.7.0",
-  "eu.timepit"      %% "refined"        % "0.9.27"
+  "eu.timepit"      %% "refined"        % "0.9.28"
 )
 
 enablePlugins(JavaAppPackaging)
--- src/main/scala/com/vladkopaniev/ci/telegram/config/config.scala
+++ src/main/scala/com/vladkopaniev/ci/telegram/config/config.scala
@@ -1,25 +1,44 @@
 package com.vladkopaniev.ci.telegram.config
 
+import cats.Show
 import cats.effect.Async
-import ciris._
-import ciris.refined._
-import eu.timepit.refined.auto._
-
+import ciris.*
+import eu.timepit.refined.api.{RefType, Validate}
+import eu.timepit.refined.auto.*
 import eu.timepit.refined.types.string.NonEmptyString
 import eu.timepit.refined.types.numeric.PosInt
+import org.tpolecat.typename.TypeName
 
 //switch to safer APIs like `ciris` once all dependencies migrate to Scala 3
 def loadBotConfig: BotAppConfig = BotAppConfig(
-    port = PosInt.unsafeFrom(sys.env.get("PORT").fold(8080)(_.toInt)),
-    databaseConfig = DatabaseConfig(url = NonEmptyString.unsafeFrom(
-        sys.env
-          .get("JDBC_DATABASE_URL")
-          .fold("jdbc:postgresql:subscribers")(identity)
-      )
-    ),
-    telegramApiConfig = TelegramApiConfig(token =  NonEmptyString.unsafeFrom(sys.env.getOrElse("BOT_API_TOKEN", "token"))),
-    webhookConfig = WebhookConfig(
-        NonEmptyString.unsafeFrom(sys.env.getOrElse("WEBHOOK_HOST_BASE_URL", "localhost")),
-        NonEmptyString.unsafeFrom(sys.env.getOrElse("WEBHOOK_REQUEST_URL_SCHEME", "https"))
+  port = PosInt.unsafeFrom(sys.env.get("PORT").fold(8080)(_.toInt)),
+  databaseConfig = DatabaseConfig(url =
+    NonEmptyString.unsafeFrom(
+      sys.env
+        .get("JDBC_DATABASE_URL")
+        .fold("jdbc:postgresql:subscribers")(identity)
     )
+  ),
+  telegramApiConfig = TelegramApiConfig(token =
+    NonEmptyString.unsafeFrom(sys.env.getOrElse("BOT_API_TOKEN", "token"))
+  ),
+  webhookConfig = WebhookConfig(
+    NonEmptyString.unsafeFrom(
+      sys.env.getOrElse("WEBHOOK_HOST_BASE_URL", "localhost")
+    ),
+    NonEmptyString.unsafeFrom(
+      sys.env.getOrElse("WEBHOOK_REQUEST_URL_SCHEME", "https")
+    )
+  )
 )
+
+implicit final def refTypeConfigDecoder[F[_, _], A, B, P](implicit
+  decoder: ConfigDecoder[A, B],
+  refType: RefType[F],
+  show: Show[B],
+  validate: Validate[B, P],
+  typeName: TypeName[F[B, P]]
+): ConfigDecoder[A, F[B, P]] = {
+  val refine = refType.refine[P]
+  decoder.mapOption(typeName.value)(refine(_).toOption)
+}
